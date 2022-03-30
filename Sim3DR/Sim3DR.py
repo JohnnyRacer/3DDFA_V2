8c392bbd9b021e94aaf12e4aeb22228c6feb6685
geminal <adamantiumgreen@gmail.com>
2022-03-30 12:28:57 -0700

    Everything has been added

--- Sim3DR/Sim3DR.py
+++ Sim3DR/Sim3DR.py
@@ -1,8 +1,20 @@
 # coding: utf-8
 
-from . import _init_paths
+
 import numpy as np
 import Sim3DR_Cython
+import os.path as osp
+import sys
+
+
+def add_path(path):
+    if path not in sys.path:
+        sys.path.insert(0, path)
+
+
+this_dir = osp.dirname(__file__)
+lib_path = osp.join(this_dir, '.')
+add_path(lib_path)
 
 
 def get_normal(vertices, triangles):
--- Sim3DR/setup.py
+++ Sim3DR/setup.py
@@ -11,9 +11,13 @@ import numpy
 setup(
     name='Sim3DR_Cython',  # not the package name
     cmdclass={'build_ext': build_ext},
+     data_files=[
+        "lib/rasterize.h"
+        
+        ],
     ext_modules=[Extension("Sim3DR_Cython",
                            sources=["lib/rasterize.pyx", "lib/rasterize_kernel.cpp"],
                            language='c++',
-                           include_dirs=[numpy.get_include()],
+                           include_dirs=['lib', numpy.get_include()],
                            extra_compile_args=["-std=c++11"])],
 )
